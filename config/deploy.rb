9748b3fc43d80f520dbe11968de92dcb4c823b0a
Nicolas Grenié <nicolas.grenie@gmail.com>
2013-09-09 16:07:40 +0200

    custom nginx conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,40 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
+require 'capistrano/foreman'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "sentimentApi"
+set :user, "azureuser"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :repository, "git@github.com:picsoung/azure_grape.git"
+set :branch, "master"
+set :use_sudo, true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+server "picsoung.cloudapp.net", :web, :app, :db, primary: true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :deploy_to, "/home/#{user}/apps/#{application}"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+ssh_options[:port] = 22
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  set :foreman_sudo, 'sudo'                    # Set to `rvmsudo` if you're using RVM
+  set :foreman_upstart_path, '/home/#{user}/apps/#{application}' # Set to `/etc/init/` if you don't have a sites folder
+  set :foreman_options, {
+    app: application,
+    log: "#{shared_path}/log",
+    user: user,
+  }
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "mkdir -p #{shared_path}/config"
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+
+  task :symlink_config, roles: :app do
+    # Add database config here
+  end
+  after "deploy:finalize_update", "deploy:fix_permissions"
+  after "deploy:finalize_update", "deploy:symlink_config"
+end
