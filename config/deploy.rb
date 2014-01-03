682cec128416e9ca6032e74e3ca5c3971940fa30
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-01-03 14:48:46 -0800

    modif conf files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,37 +1,43 @@
 require "bundler/capistrano"
-require 'capistrano/foreman'
 
-set :application, "sentimentApi"
+
+set :application, "grape"
 set :user, "azureuser"
+
 
 set :scm, :git
 set :repository, "git@github.com:picsoung/azure_grape.git"
 set :branch, "master"
 set :use_sudo, true
 
-server "picsoung.cloudapp.net", :web, :app, :db, primary: true
+
+server "rubytest3scale.cloudapp.net", :web, :app, :db, primary: true
+
 
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:port] = 22
 
+
 namespace :deploy do
   desc "Fix permissions"
   task :fix_permissions, :roles => [ :app, :db, :web ] do
-    run "chmod +x #{release_path}/config/config.sh"
+    run "chmod +x #{release_path}/config/unicorn_init.sh"
   end
 
-  set :foreman_sudo, 'sudo'                    # Set to `rvmsudo` if you're using RVM
-  set :foreman_upstart_path, '/home/#{user}/apps/#{application}' # Set to `/etc/init/` if you don't have a sites folder
-  set :foreman_options, {
-    app: application,
-    log: "#{shared_path}/log",
-    user: user,
-  }
+
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "service unicorn_#{application} #{command}"
+    end
+  end
+
 
   task :setup_config, roles: :app do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     sudo "mkdir -p #{shared_path}/config"
   end
   after "deploy:setup", "deploy:setup_config"
@@ -41,5 +47,5 @@ namespace :deploy do
     # Add database config here
   end
   after "deploy:finalize_update", "deploy:fix_permissions"
-  #after "deploy:finalize_update", "deploy:symlink_config"
+  after "deploy:finalize_update", "deploy:symlink_config"
 end
