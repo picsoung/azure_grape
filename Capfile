6d30239cd2e910341a31d3714bc038c49ba0c58f
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-01-03 16:53:05 -0800

    update in conf

--- Capfile
+++ Capfile
@@ -1,4 +1,6 @@
 load 'deploy'
 # Uncomment if you are using Rails' asset pipeline
-#load 'deploy/assets'
+#load 'deploy/assets' no assets in GRAPE
 load 'config/deploy' # remove this line to skip loading any of the default tasks
+
+# run cap deploy:update deploy:start to avoid migration
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,3 @@
-worker_processes 2
-timeout 30
-listen "/tmp/unicorn.blog.sock"
- 
 root = "/home/azureuser/apps/grape/current"
  
 working_directory root
@@ -9,3 +5,7 @@ working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
+
+worker_processes 2
+timeout 30
+listen "/tmp/unicorn.grape.sock"
